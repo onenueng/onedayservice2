5ab4e9c9a61788787ebc73f4fd56ec9e8a50cad0
onenueng <saipin.plu@mahidol.edu>
2022-09-29 23:38:30 +0700

    Day4

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+    protected $fillable = ['datetime_start', 'time', 'bed_id', 'procedure_id'];
 }
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+    protected $fillable = ['name_short','name','active'];
+
 }
